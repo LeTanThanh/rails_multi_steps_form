75024d59490f3b231e35d3ad9cc0aa1a1615df93
LeTanThanh <tanthanhcr95@gmail.com>
2019-04-19 22:17:25 +0700

    add_validates

--- app/models/shipping.rb
+++ app/models/shipping.rb
@@ -1,2 +1,6 @@
 class Shipping < ApplicationRecord
+  validates :receiver_name, presence: true
+  validates :receiver_phone, presence: true
+  validates :shipping_address, presence: true
+  validates :shipping_day, presence: true
 end
